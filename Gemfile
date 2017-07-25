96fe293172c972ca5fc89a9ccbaf47d6fef35e5a
Jordan Hudgens <jordan@devcamp.com>
2017-07-25 14:57:28 +0000

    Dev tool initial commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+# Specify your gem's dependencies in gilman_view_tool.gemspec
+gemspec
---
+++ lib/gilman_view_tool.rb
@@ -0,0 +1,6 @@
+require "gilman_view_tool/version"
+require "gilman_view_tool/renderer"
+
+module GilmanViewTool
+  # Your code goes here...
+end
